98848a8baa920a8caa04664e9e255ed46bdb0b05
JessyCuevas <jessy.cuevas.aranda@gmail.com>
2017-12-04 18:51:48 -0600

    Contador con función

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
 var boxText = document.getElementById("boxText");
 var wordCount = document.getElementById("wordCount");
+var button = document.getElementById("nt-submit");
 
 boxText.addEventListener("keyup",function(){
 	var characters = boxText.value.split('',140);
   wordCount.innerText = characters.length;
 });
 
+boxText.addEventListener("keyup", counter);
+
 
 function counter() {
-	 var characters = textArea.value.split('');
-    counterNumber.innerText = 140 - (characters.length);
+	 var characters = boxText.value.split('');
+	 console.log(characters.length);
+	 wordCount.innerText = 140 - (characters.length);
     if (characters.length >= 121 && characters.length < 130) {
-        counterNumber.style.color = 'yellow';
+        wordCount.style.color = 'yellow';
     } else if (characters.length >= 131 && characters.length < 140) {
-        counterNumber.style.color = 'red';
+        wordCount.style.color = 'red';
     } else if (characters.length < 120) {
-        counterNumber.style.color = 'black';
+        wordCount.style.color = 'black';
     }
     if (characters.length > 0) {
         button.removeAttribute('disabled')
         button.style.backgroundColor = 'blue';
     }
-    if (textArea.value == '' || textArea.value[0] == "\n" || characters.length > 140) {
+    if (boxText.value == '' || boxText.value[0] == "\n" || characters.length > 140) {
         button.setAttribute('disabled', 'true');
         button.style.backgroundColor = 'grey';
     }
+}
+//counter();
+
 //}
 //Fin de la conversación de chat
 //Escribe un mensaje...
